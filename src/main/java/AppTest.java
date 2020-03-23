4e9c1f179e1229da06a78f2999111791eb747412
liuhuan <837444118@qq.com>
2020-03-23 14:44:14 +0800

    小马哥第一次创建

---
+++ src/main/java/AppTest.java
@@ -0,0 +1,19 @@
+import com.sun.org.apache.xpath.internal.res.XPATHErrorResources_it;
+
+public class AppTest {
+    int a = 1;
+
+    @Override
+    public String toString() {
+        return "AppTest{" +
+                "a=" + a +
+                '}';
+    }
+
+    //    public static  void main(String args[]){
+//
+//    }args
+    public static void main(String[] args) {
+        System.out.println("1223");
+    }
+}
